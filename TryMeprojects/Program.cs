c8cde4f5e6b3925ae9af33e4987aec304f73486e
nkprasanna <nkprasasnna@gmail.com>
2016-08-18 10:50:36 +0530

    Optional parameter

---
+++ TryMeprojects/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+
+namespace TryMeprojects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           OptionalParameter. OptionalParameterMethod(20,"prasanna");
+        }
+       
+    }
+    public class OptionalParameter
+    {
+       public static void OptionalParameterMethod(int Cal, string Name = "No Name", bool IsEdit = false)
+        {
+
+            Console.WriteLine("Print Values:{0} Name: {1} Edit: {2}", Cal, Name, IsEdit);
+        }
+    }
+}
